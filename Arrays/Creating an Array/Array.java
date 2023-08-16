e67417c2ea3b642ed71812b41729ab98e67b35a1
Yash Katiyar <92661124+yashkatiyar2503@users.noreply.github.com>
2023-08-17 01:11:37 +0530

    Create Array.java

---
+++ Arrays/Creating an Array/Array.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Array
+{
+    public static void main(String[] args) {
+        int m[];
+        m=new int[5]; //method 1 to declare and initialize an array
+        int n[]=new int[5]; //method 2 to declare and initialize an array
+        int a[]={100,50,70,80,25}; //declaration and initialization of an array
+    }
+}
