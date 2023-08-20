211e68b369d62b32bd87ce1037b6afe700032da0
Yash Katiyar <92661124+yashkatiyar2503@users.noreply.github.com>
2023-08-21 01:55:35 +0530

    Update Array.java

--- Arrays/Creating an Array/Array.java
+++ Arrays/Creating an Array/Array.java
@@ -3,8 +3,11 @@ public class Array
 {
     public static void main(String[] args) {
         int m[];
-        m=new int[5]; //method 1 to declare and instantiate an array
-        int n[]=new int[5]; //method 2 to declare and instantiate an array
-        int a[]={100,50,70,80,25}; //declaration and initialization of an array
+        m=new int[5]; // Declare + Instantiate an array - Method 1
+        int n[]=new int[5]; //Declare + Instantiate an array - Method 2
+        int a[]={100,50,70,80,25}; // Declare + Initialize an array
+        for (int b: a) {
+            System.out.println(b);
+        } //Print an array
     }
 }
