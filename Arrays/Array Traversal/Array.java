9eed6543a96ac31418b74d42d9454cd0d87eb817
Yash Katiyar <92661124+yashkatiyar2503@users.noreply.github.com>
2023-08-29 00:22:08 +0530

    Add files via upload

---
+++ Arrays/Array Traversal/Array.java
@@ -0,0 +1,8 @@
+public class Array {
+    public static void main(String[] args) {
+        int[] arr = { 1, 2, 3, 4, 5 };
+        for (int i = 0; i < arr.length; i++) { // traversing through the array
+            System.out.println(arr[i]);
+        }
+    }
+}
